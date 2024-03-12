2bd53e21dadee233f84c6f229be2e788883b4982
yltaba <yuri.l.taba@gmail.com>
2024-03-12 19:27:22 -0300

    adj

--- app_consolidacao_fcst_sga.py
+++ app_consolidacao_fcst_sga.py
@@ -7,16 +7,15 @@ st.title('Consolidador - Forecast SGA')
 uploaded_files = st.file_uploader("Selecione os arquivos para consolidação", 
                                   accept_multiple_files=True)
 
-dfs = []
+sga_forecast = pd.DataFrame()
 if uploaded_files is not None:
     for uploaded_file in uploaded_files:
 
         dataframe = pd.read_excel(uploaded_file, 
                                   sheet_name='CargaDW_ForecastBR')
 
-        dfs.append(dataframe)
+        sga_forecast = pd.concat([sga_forecast, dataframe], ignore_index=True)
 
-sga_forecast = pd.concat(dfs, ignore_index=True)
 # towrite = BytesIO()
 # sga_forecast.to_excel(towrite, index=False)
 # towrite.seek(0)
